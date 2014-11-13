0921dcb21deac7da0d4d8965bfac9f9233b8e985
Ruocheng Li <lrc.bill@gmail.com>
2014-11-13 00:26:40 -0800

    add syscall for alarm

---
+++ alarmtest.c
@@ -0,0 +1,25 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void periodic(void);
+
+int
+main(int argc, char *argv[])
+{
+  int i;
+  printf(1, "alarmtest starting\n");
+  alarm(10, (void *)periodic);
+  for(i = 0; i < 50*500000; i++){
+    if((i++ % 500000) == 0)
+      write(2, ".", 1);
+  }
+  exit();
+  return 0;
+}
+
+void
+periodic(void)
+{
+  printf(1, "alarm!\n");
+}
--- sysproc.c
+++ sysproc.c
@@ -110,3 +110,18 @@ sys_halt(void)
   return 0;
 }
 
+int
+sys_alarm(void)
+{
+   int ticks;
+   void (*handler)();
+
+   if(argint(0, &ticks) < 0)
+     return -1;
+   if(argptr(1, (char**)&handler, 1) < 0)
+     return -1;
+   proc->alarmticks = ticks;
+   proc->alarmhandler = handler;
+   return 0;
+}
+
